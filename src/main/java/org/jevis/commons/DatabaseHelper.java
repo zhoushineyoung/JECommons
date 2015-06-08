fb64fb4978e312c461fefe9ece59570cc3a44f39
fredrich <fredrich@ukshikmb-cl048.i-kmb.de>
2015-06-08 17:40:55 +0200

    FEATURE: first JEReport Version adjust to new API

--- src/main/java/org/jevis/commons/DatabaseHelper.java
+++ src/main/java/org/jevis/commons/DatabaseHelper.java
@@ -5,8 +5,13 @@
 package org.jevis.commons;
 
 import org.jevis.api.JEVisException;
+import org.jevis.api.JEVisFile;
 import org.jevis.api.JEVisObject;
+import org.jevis.api.JEVisSelection;
 import org.jevis.api.JEVisType;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 /**
  *
@@ -17,6 +22,22 @@ public class DatabaseHelper {
     public static boolean checkValidStringObject(JEVisObject jevisObject, JEVisType jevisType) throws JEVisException {
         boolean isValid = false;
         if (jevisObject.getAttribute(jevisType).hasSample() && jevisObject.getAttribute(jevisType).getLatestSample() != null && jevisObject.getAttribute(jevisType).getLatestSample().getValueAsString() instanceof String && !jevisObject.getAttribute(jevisType).getLatestSample().getValueAsString().equals("")) {
+            isValid = true;
+        }
+        return isValid;
+    }
+
+    private static boolean checkValidFileObject(JEVisObject jevisObject, JEVisType jevisType) throws JEVisException {
+        boolean isValid = false;
+        if (jevisObject.getAttribute(jevisType).hasSample() && jevisObject.getAttribute(jevisType).getLatestSample() != null && jevisObject.getAttribute(jevisType).getLatestSample().getValueAsFile() instanceof JEVisFile) {
+            isValid = true;
+        }
+        return isValid;
+    }
+
+    private static boolean checkValidSelectionObject(JEVisObject jevisObject, JEVisType jevisType) throws JEVisException {
+        boolean isValid = false;
+        if (jevisObject.getAttribute(jevisType).hasSample() && jevisObject.getAttribute(jevisType).getLatestSample() != null && jevisObject.getAttribute(jevisType).getLatestSample().getValueAsFile() instanceof JEVisSelection) {
             isValid = true;
         }
         return isValid;
@@ -78,4 +99,41 @@ public class DatabaseHelper {
         }
         return value;
     }
+
+    public static JEVisFile getObjectAsFile(JEVisObject jevisObject, JEVisType jevisType) throws JEVisException {
+        JEVisFile value = null;
+        try {
+            if (DatabaseHelper.checkValidFileObject(jevisObject, jevisType)) {
+                value = jevisObject.getAttribute(jevisType).getLatestSample().getValueAsFile();
+
+            }
+        } catch (NumberFormatException | JEVisException nfe) {
+        }
+        return value;
+    }
+
+    public static DateTime getObjectAsDate(JEVisObject jevisObject, JEVisType jevisType, DateTimeFormatter timeFormat) throws JEVisException {
+        String value = null;
+        try {
+            if (DatabaseHelper.checkValidStringObject(jevisObject, jevisType)) {
+                value = jevisObject.getAttribute(jevisType).getLatestSample().getValueAsString();
+
+            }
+        } catch (NumberFormatException | JEVisException nfe) {
+        }
+        DateTime datetime = timeFormat.parseDateTime(value);
+        return datetime;
+    }
+
+    public static JEVisSelection getObjectAsSelection(JEVisObject jevisObject, JEVisType jevisType) throws JEVisException {
+        JEVisSelection value = null;
+        try {
+            if (DatabaseHelper.checkValidSelectionObject(jevisObject, jevisType)) {
+                value = jevisObject.getAttribute(jevisType).getLatestSample().getValueAsSelection();
+            }
+        } catch (NumberFormatException | JEVisException nfe) {
+        }
+        return value;
+    }
+
 }
